8402a710363422699c42b72bbf403f5734dd6356
ZakhilAmin <amingull@gmail.com>
2020-10-04 16:54:57 +0430

    designed navbar, login and backend for the project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,56 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
+import { AngularFireModule} from 'angularfire2';
+import {AngularFireDatabaseModule} from 'angularfire2/database';
+import { AngularFireAuthModule} from 'angularfire2/auth';
+import { RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { HomeComponent } from './home/home.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProductComponent } from './product/product.component';
+import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from './check-out/check-out.component';
+import { OrderSuccessComponent } from './order-success/order-success.component';
+import { MyOrderComponent } from './my-order/my-order.component';
+import { AdminProductsComponent } from './admin/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './admin/admin-orders/admin-orders.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent,
+    HomeComponent,
+    ProductComponent,
+    ShoppingCartComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    MyOrderComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule,
+    RouterModule.forRoot([
+      {path:'', component:HomeComponent},
+      {path:'products', component: ProductComponent},
+      {path:'shopping-cart', component:ShoppingCartComponent},
+      {path: 'check-out',component:CheckOutComponent},
+      {path: 'order-success',component:OrderSuccessComponent},
+      {path: 'login',component:LoginComponent},
+      {path: 'my/orders',component:MyOrderComponent},
+      {path: 'admin/products',component:AdminProductsComponent},
+      {path: 'admin/orders',component:AdminOrdersComponent},
+     
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyDY0Q13SgDrjVMSY4ZMw2CTVcdo_ZBEOo4",
+    authDomain: "oshop-47bc8.firebaseapp.com",
+    databaseURL: "https://oshop-47bc8.firebaseio.com",
+    projectId: "oshop-47bc8",
+    storageBucket: "oshop-47bc8.appspot.com",
+    messagingSenderId: "543380496760",
+    appId: "1:543380496760:web:6b4d4f36a5dc467762e88d",
+    measurementId: "G-ZQQYV5549E"
+  }
 };
 
 /*
